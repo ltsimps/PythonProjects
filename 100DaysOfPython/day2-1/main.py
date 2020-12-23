d99665d1feb9351283d5687b4724485ea9326801
ltsimps <ltsimpso@me.com>
2020-12-23 08:22:00 -0500

    Day two Python

---
+++ 100DaysOfPython/day2-1/main.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+print(int(two_digit_number[0])+ int(two_digit_number[1]) )
+
